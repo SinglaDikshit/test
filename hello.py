cce1c69615113ab38fd19811e59cef4b7689f78c
SinglaDikshit <166998998+SinglaDikshit@users.noreply.github.com>
2024-07-24 16:44:25 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -5,14 +5,7 @@ import pandas as pd
 st.header("Test")
 
 # Example usage of prophet
-df = pd.DataFrame({
-    'ds': pd.date_range(start='2020-01-01', periods=30),
-    'y': range(30)
-})
-
+df = pd.read_csv("ZOMATO.NS.csv")
 m = Prophet()
 m.fit(df)
-future = m.make_future_dataframe(periods=10)
-forecast = m.predict(future)
-
-st.write(forecast)
+st.write("forecast")
